579383036a3f89c381aeec639707b2fcb65fe8d1
alperduran217 <alperduran217@gmail.com>
2017-10-11 23:44:35 +0200

    first commit

---
+++ Auto-Layout-Exercise/Auto-Layout-Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto-Layout-Exercise
+//
+//  Created by Mehmet Alper Duran on 11.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Auto-Layout-Exercise/Auto-Layout-Exercise/Views/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  Auto-Layout-Exercise
+//
+//  Created by Mehmet Alper Duran on 11.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        layer.borderColor = UIColor.black.cgColor
+        
+    }
+
+}

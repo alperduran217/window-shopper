6aa478feedae4603139dda9ac4858ded5043e3d6
alperduran217 <alperduran217@gmail.com>
2017-10-12 11:21:05 +0200

    the project is finished

---
+++ window-shopper/window-shopper/Model/Wage.swift
@@ -0,0 +1,9 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Mehmet Alper Duran on 12.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import Foundation
---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Mehmet Alper Duran on 11.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
